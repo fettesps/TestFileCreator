db915dd3d9cd470d098098d1b30abfbf1ba58a32
Brad Fettes <brad.fettes@orchestry.com>
2023-07-04 18:03:10 -0400

    Abstracted GenerateFile() method and installed System.IO.Abstractions to allow for mocking out of FileSystem in order to test file generation methods.

--- Test File Creator/Program.cs
+++ Test File Creator/Program.cs
@@ -1,3 +1,5 @@
+using System.IO.Abstractions;
+
 namespace Test_File_Creator
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Test_File_Creator
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmMain());
+            Application.Run(new frmMain(new FileSystem()));
         }
     }
 }
